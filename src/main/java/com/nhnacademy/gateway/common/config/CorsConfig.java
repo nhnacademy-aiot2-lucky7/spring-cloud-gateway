a33a9d6eed56d7c37e058291711346fe346de3ab
nink2458 <nink2458@gmail.com>
2025-04-14 13:07:06 +0900

    init

---
+++ src/main/java/com/nhnacademy/gateway/common/config/CorsConfig.java
@@ -0,0 +1,9 @@
+package com.nhnacademy.gateway.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfig implements WebFluxConfigurer {
+
+}
