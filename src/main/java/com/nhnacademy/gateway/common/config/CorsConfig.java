d99e78a3dc421b7c5663a4593c9f46a05fc95507
st120712 <zzw123@naver.com>
2025-06-04 22:19:05 +0900

    cors config rollback

--- src/main/java/com/nhnacademy/gateway/common/config/CorsConfig.java
+++ src/main/java/com/nhnacademy/gateway/common/config/CorsConfig.java
@@ -2,31 +2,53 @@ package com.nhnacademy.gateway.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 import java.util.List;
 
+/**
+ * CORS 정책을 설정하는 Config 영역입니다.
+ */
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebFluxConfigurer {
 
     @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration config = new CorsConfiguration();
+    public CorsWebFilter corsFilter() {
+        return new CorsWebFilter(corsConfigurationSource());
+    }
 
-        // 허용할 origin 추가
-        config.addAllowedOrigin("https://luckyseven.live");
-        config.addAllowedOrigin("https://www.luckyseven.live");
-        config.addAllowedOrigin("http://localhost:10231");
+    /**
+     * 특정 출처(origin)에서 오는 해당하는 HTTP Method 요청만 허용하도록 설정된 CORS 정책을 반환합니다.
+     */
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
 
-        config.setAllowCredentials(true);
-        config.setAllowedHeaders(List.of("Content-Type", "X-USER-ID", "Authorization"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        // cross-origin 요청이 허용되는 출처를 설정하십시오.
+        corsConfig.setAllowedOriginPatterns(List.of("*"));
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
+        corsConfig.setAllowedHeaders(List.of("Content-Type", "X-USER-ID", "Authorization"));
+        corsConfig.setAllowCredentials(true);
 
-        return new CorsWebFilter(source);
+        // 요청으로 허용하는 HTTP Methods를 설정하십시오.
+        corsConfig.setAllowedMethods(
+                List.of(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
+        );
+
+        // 커스텀한 설정 값을 path에 매핑
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return source;
     }
 }
