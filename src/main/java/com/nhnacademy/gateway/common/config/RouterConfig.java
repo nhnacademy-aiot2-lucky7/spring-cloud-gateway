ea3c2871a2fb2442f23cec13409956781e50be40
st120712 <zzw123@naver.com>
2025-06-04 11:13:08 +0900

    .

--- src/main/java/com/nhnacademy/gateway/common/config/RouterConfig.java
+++ src/main/java/com/nhnacademy/gateway/common/config/RouterConfig.java
@@ -65,7 +65,7 @@ public class RouterConfig {
                         "AUTH-SERVICE",
                         r -> r
                                 .path("/auth/**")
-                                .uri("lb://AUTH-SERVICE")
+                                .uri("lb://auth-service")
                 )
                 .route(
                         "EVENT-SERVICE",
