fbcc9370300026d54fbb9fb7d45869a9293c16ef
Kimsongbeen <ksb10266@naver.com>
2025-06-05 11:25:53 +0900

    dashboard_설정추가

--- src/main/java/com/nhnacademy/gateway/common/config/RouterConfig.java
+++ src/main/java/com/nhnacademy/gateway/common/config/RouterConfig.java
@@ -38,6 +38,13 @@ public class RouterConfig {
                                 .uri("lb://USER-SERVICE")
                 )
                 .route(
+                        "DASHBOARD-SERVICE",
+                        r -> r
+                                .path("/api/folders/**", "/api/dashboards/**", "/api/panels/**")
+                                .filters(f -> f.filter(jwtAuthorizationFilter))
+                                .uri("lb://DASHBOARD-SERVICE")
+                )
+                .route(
                         "AUTH-SERVICE",
                         r -> r
                                 .path("/auth/**")
